f185e52b609a6ba5b237a0e09e52cbbdb17a4dd1
EBoy9696 <ledaigiadevilgamer@gmail.com>
2025-08-04 16:42:31 +0700

    feat(gameplay): add TowersManager ScriptableObject and configure GamePlay scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,31 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GameManager : Singleton<GameManager>
 {
-    public bool Loggedin = false;
+    public UIDocument document;
+    public Camera Camera;
+    public GameObject InitialPos;
+    public GameObject TargetPos;
+    public VisualTreeAsset towersBar;
+    public VisualTreeAsset towersPanel;
+
+    public VisualElement rootVisualElement;
+
+    private void Start()
+    {
+        rootVisualElement = document.rootVisualElement;
+        Invoke("moveScreen", 1f);
+    }
+
+    private void updateUI()
+    {
+        var newScreenInstance = towersBar.CloneTree();
+
+    }
+    
+    private void moveScreen()
+    {
+
+    }
 }
